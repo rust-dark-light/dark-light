912fe8a8e66cfde6bd1d09b8ca34ee791a6a6dae
Be <be.0@gmx.com>
2025-01-06 15:19:32 -0600

    remove sync::subscribe
    This is just wrapping async code and I don't think there's much
    of a use case for it. Anyone who wants this can start their own
    async executor and use the async fn subscribe.

--- src/platforms/freedesktop/subscribe.rs
+++ src/platforms/freedesktop/subscribe.rs
@@ -5,37 +5,6 @@ use futures_lite::{Stream, StreamExt};
 
 use crate::Mode;
 
-#[cfg(any(feature = "sync", doc))]
-pub(crate) mod sync {
-    use super::super::Mode;
-    use super::color_scheme_stream;
-    use futures_lite::StreamExt;
-
-    pub fn subscribe() -> std::sync::mpsc::Receiver<Mode> {
-        let (tx, rx) = std::sync::mpsc::channel();
-
-        std::thread::spawn(move || {
-            futures_lite::future::block_on(async {
-                let stream = match color_scheme_stream().await {
-                    Ok(stream) => stream.boxed(),
-                    Err(err) => {
-                        log::error!("Failed to subscribe to color scheme changes: {}", err);
-                        futures_lite::stream::empty().boxed()
-                    }
-                };
-
-                stream
-                    .for_each(|mode| {
-                        let _ = tx.send(mode);
-                    })
-                    .await;
-            });
-        });
-
-        rx
-    }
-}
-
 pub async fn subscribe() -> impl Stream<Item = Mode> + Send {
     match color_scheme_stream().await {
         Ok(stream) => stream.boxed(),
--- src/platforms/websys/subscribe.rs
+++ src/platforms/websys/subscribe.rs
@@ -1,26 +1,3 @@
-#[cfg(any(feature = "sync", doc))]
-pub(crate) mod sync {
-    pub fn subscribe() -> std::sync::mpsc::Receiver<crate::Mode> {
-        let (tx, rx) = std::sync::mpsc::channel();
-        let mut last_mode = crate::sync::detect();
-
-        tx.send(last_mode).unwrap();
-
-        std::thread::spawn(move || loop {
-            let current_mode = crate::sync::detect();
-
-            if current_mode != last_mode {
-                if tx.send(current_mode).is_err() {
-                    break;
-                }
-                last_mode = current_mode;
-            }
-        });
-
-        rx
-    }
-}
-
 pub async fn subscribe() -> impl futures_lite::Stream<Item = crate::Mode> {
     Box::pin(futures_lite::stream::unfold(
         crate::detect().await,
