d3bea7202a1b5e15a9a5332ff8a363861b21e428
Be <be.0@gmx.com>
2025-01-06 14:24:29 -0600

    impl From<bool> for Mode
    Use the std API instead of inventing our own

--- src/platforms/websys/detect.rs
+++ src/platforms/websys/detect.rs
@@ -2,7 +2,7 @@ pub fn detect_mode() -> crate::Mode {
     if let Some(window) = web_sys::window() {
         let query_result = window.match_media("(prefers-color-scheme: dark)");
         if let Ok(Some(mql)) = query_result {
-            return crate::Mode::from_bool(mql.matches());
+            return mql.matches().into();
         }
     }
     crate::Mode::Light
